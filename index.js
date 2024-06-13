caee678044a5e7b75c6549877477cdabbe620053
JoaoFabioMA <amarall.jf@gmail.com>
2024-06-13 13:20:26 -0400

    criado rota Sobre

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/sobre', (req, res) => {
+    res.json({
+        title: 'Sobre a aplicação',
+        message: 'projeto aula9317412',
+        author: 'JoaoFabioMA',
+		version: '0.0.1'
+    })
+})
+
 app.listen(process.env.PORT || PORT, () => {
     console.log('Servidor rodando em http://localhost:3000');
 })
